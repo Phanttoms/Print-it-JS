39a53b8f89c275ba5357d666037c4d7dd9f20647
Phanttoms <yohan.pruche@orange.fr>
2023-11-28 14:52:18 +0100

    projet-complet

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,63 @@ const slides = [
 		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
 	},
 ];
+
+// Variable
+let activeIndex = 0;
+
+// Creation des bullets points
+function createDot() {
+	const dotsContainer = document.querySelector(".dots");
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("div");
+		dot.classList.add("dot");
+		dotsContainer.appendChild(dot);
+		if (i === 0) {
+			dot.classList.add("dot_selected");
+		}
+	}
+}
+
+createDot();
+
+// Récuperation et Listener des fléches gauche et droite
+document.getElementById("leftArrow").addEventListener("click", () => {
+	activeIndex--;
+	displaySlide(activeIndex);
+	console.log("leftArrow");
+});
+
+document.getElementById("rightArrow").addEventListener("click", () => {
+	activeIndex++;
+	displaySlide(activeIndex);
+	console.log("rightArrow");
+});
+
+// Récuperation et mise à jour des images
+function displaySlide(index) {
+	if (index < 0) {
+		activeIndex = slides.length - 1;
+	} else if (index >= slides.length) {
+		activeIndex = 0;
+	}
+	const slide = slides[activeIndex];
+	const slideImage = document.getElementById("imgBanner");
+	const tagLine = document.getElementById("imgText");
+
+	slideImage.src = `./assets/images/slideshow/${slide.image}`;
+	tagLine.innerHTML = slide.tagLine;
+	updateDot(activeIndex);
+}
+
+// Mise à jour des bullets
+function updateDot() {
+	const dots = document.querySelectorAll(".dot");
+
+	dots.forEach((dot, index) => {
+		if (index === activeIndex) {
+			dot.classList.add("dot_selected");
+		} else {
+			dot.classList.remove("dot_selected");
+		}
+	});
+}
